09f84a87e6fa5ba86e71a09a7717602c28d36f1d
Igor Lima <igorlimaisl7@gmail.com>
2025-03-05 17:34:26 -0300

    aula.js

---
+++ variaveis_e_constantes/18loops.js
@@ -0,0 +1,10 @@
+
+let lista = [ "joaa","maria ","otavio" ,"maria","anderson"]
+for(let i = 0; i <  5; i = i + 1){
+   //console.log('O elemento da vez é ')
+   //console.log(lista[i])
+}
+
+for(let i = 0; i < lista.length; i = i +1){
+    console.log(lista[i])
+}
