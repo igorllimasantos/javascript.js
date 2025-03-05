6c75eb45fa941cc356a003df8184c321f4659e4f
Igor Lima <igorlimaisl7@gmail.com>
2025-03-04 21:46:36 -0300

    javascript

---
+++ 06exercicio.js
@@ -0,0 +1,9 @@
+let nomeDoprogramador = "Igor lima"
+let sobrenomeDoprogramador = "Dos Santos"
+
+let salarioAnual = 1600 * 12
+
+
+let nomeCompleto = nomeDoprogramador + sobrenomeDoprogramador
+console.log(nomeCompleto)
+console.log(salarioAnual)
